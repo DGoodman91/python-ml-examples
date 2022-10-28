7ece6cac3cb717a30b866444160f1c76ed7f16fd
GoodmanDev <david.g.goodman@bath.edu>
2022-10-28 10:29:27 -0600

    Add r-squared calculation for polynomial regression analysis

--- simple-statistics/distributions/scatter-with-polynomial-regression.py
+++ simple-statistics/distributions/scatter-with-polynomial-regression.py
@@ -6,6 +6,7 @@
 import numpy
 import matplotlib.pyplot as pyplot
 from scipy import stats
+from sklearn.metrics import r2_score
 
 xdata = [1, 2, 3, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 18, 19, 21, 22]
 ydata = [100, 90, 80, 60, 60, 55, 60, 65,
@@ -25,6 +26,14 @@ poly_model = numpy.poly1d(numpy.polyfit(xdata, ydata, polynomial_degree))
 
 # the third argument is the number of data points which will be generated
 poly_line = numpy.linspace(min(xdata), max(xdata), 100)
+
+# similar to the linear analysis r value, we can calculate an "r-squared" value for our polynomial regression analysis
+# between 0 & 1, 1 from no-correlation to complete-correlation
+print("r-squared value when attempting polynomial regression analysis of degree {} is {}".format(polynomial_degree, r2_score(ydata, poly_model(xdata))))
+
+# we can see the r-squared value with a degree-3 polynomial regression is high (0.94) we can use the poly_model model to
+# predict the y value for any x value
+print("our model predicts a y value for an x of 17 to be {}".format(poly_model(17)))
 
 pyplot.scatter(xdata, ydata)
 pyplot.plot(xdata, line_of_linear_regression)
